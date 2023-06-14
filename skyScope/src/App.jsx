8129c0aca881d1f786a9eaec7552985e02f0f0e4
anouar_faraji <faraji.anouarr@gmail.com>
2023-06-14 18:31:45 +0100

    first commit

---
+++ skyScope/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+       <h1 className='text-3xl font-extrabold text-blue-500 hover:text-green-600'>yew wasssup bitch !!</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
