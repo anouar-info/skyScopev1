d422c65757f75b4b5553f58dc5e7ded3ba437ce0
anouar_faraji <faraji.anouarr@gmail.com>
2023-12-07 23:58:24 +0100

    remove hosting

--- skyScope/vite.config.js
+++ skyScope/vite.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { outDir:"build",manifest: true },
-  base: "/",
+  base: "/src",
   root: "./src",
   plugins: [reactRefresh()],
 });
